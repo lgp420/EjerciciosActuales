7012a1c8fb8272229de4472220ca998867ae3d28
lgp420 <palacioslucas95@gmail.com>
2024-09-28 23:22:59 -0300

    Bucles ej6 y html

--- script.js
+++ script.js
@@ -305,10 +305,148 @@ function elevarAlCubo(num) {
   // 3 ---> 27
   // 0 ---> 0
   // Tu código:
+  return Math.pow(num,3);
+}
+
+console.log(elevarAlCubo(8));
+
+//Ejercicio 33
+function elevar(num, exponent) {
+  // La función recibe dos argumentos llamados "num" y "exponent" los cuales son numeros.
+  // Debes Retorna el valor de "num" elevado al "exponent".
+  // Por ejemplo: 
+  // 2, 2 ---> 4
+  // 0, 5 ---> 0
+  // Tu código:
+  return Math.pow(4,4);
+}
+console.log(elevar());
+
+
+//Ejercicio 34
+function redondearNumero(num) {
+  // La función recibe un argumento llamado num el cual es un numero.
+  // Debes redondear "num" al entero más próximo y retornarlo.
+  // Por ejemplo: 
+  // 1.5 ---> 2
+  // 0.1 ---> 0
+  // Tu código:
+  return Math.round(num);
+}
+console.log(redondearNumero(-1.7555555));
+
+//Ejercicio 35
+function redondearHaciaArriba(num) {
+  // La función recibe un argumento llamado num el cual es un numero.
+  // Debes redondear "num" hacia arriba y retórnalo.
+  // Por ejemplo: 
+  // 2.5 ---> 3
+  // 0.1 ---> 1
+  // Tu código:
+  return Math.ceil(num);
+}
+  console.log(redondearHaciaArriba(-1.9));
+
+  //Ejercicio 36
+function numeroRandom() {
+  // La función numeroRandom debe generar un número al azar entre 0 y 1 y retornarlo.
+  // Tu código:
+  return Math.round(Math.random());
+  
+}
+console.log(numeroRandom());
+ 
+//BUCLES
+
+//Ejercicio 1
+
+function esIgualYNegativo(a, b) {
+  // La función recibe dos argumentos "a" y "b" que son números.
+  // Determina si son iguales y son ambos negativos.
+  // De ser así, retorna true, de lo contrario, retorna false.
+  // Tu código:
+  if (a == b && a<0){
+    return true;
+  } else{
+    return false;
+  }
+}
+console.log(esIgualYNegativo(-3,-3)); 
+
+//Ejercicio 2
+function esVerdaderoYFalso(booleano1, booleano2) {
+  // La función recibe dos argumentos "booleano1" y "booleano2".
+  // Retorna true si ambos son verdaderos, caso contrario, retorna false.
+  // Tu código:
+  if (booleano1 == booleano2){
+  return true;
+  } else{
+    return false;
+  }
+}
+console.log(esVerdaderoYFalso(false, true));
+
+//Ejercicio 3
+function obtenerMayor(x, y) {
+  // "x" e "y" son números enteros.
+  // Retorna el número más grande.
+  // Si son iguales, retornar cualquiera de los dos.
+  // Tu código:
+  if(x<y){
+    console.log("El mas grande es y: " + y);
+  } else if (x>y){
+    console.log("El mas grande es x: " + x);
+  } else if (x==y){
+    return x || y;
+  }
+}
+console.log(obtenerMayor(10, 15));
+
+//Ejercicio 4
+function mayoriaDeEdad(edad) {
+  // Determinar si la persona puede ingresar al evento según su edad.
+  // Si tiene 18 años o más, retorna el string: "Allowed".
+  // Caso contrario: "Not allowed".
+  // Tu código:
+  if(edad >= 18){
+    console.log("Allowed");
+  }else{
+    console.log("Not Allowed");
+  }
 
 }
+console.log(mayoriaDeEdad(18));
  
+//Ejercicio 5
+function esVerdadero(valor) {
+  // Si "valor" es verdadero, retorna "Soy verdadero".
+  // Caso contrario, retorna "Soy falso".
+  // Tu código:
+  if (valor === true){
+    console.log("Soy verdadero.");
+  } else{
+    console.log("Soy falso.");
+  }
+}
+console.log(esVerdadero(true));
+
+//Ejercicio 6
+function tieneTresDigitos(num) {
+  // Si el número recibido tiene tres dígitos, retorna true.
+  // Caso contrario, retorna false.
+  // Tu código:
+
+let digitos = num.length;
+
+if (digitos === 3) {
+  return true;
+} else {
+  return false;
+}
+
+console.log(digitos); // 3
   
   
 
-
+}
+console.log(tieneTresDigitos(123));
